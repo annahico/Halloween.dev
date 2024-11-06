0934d6401662938fa09b61024b097c1b7ad567dd
annahico <annahico@gmail.com>
2024-11-06 18:36:11 +0100

    feat: started reto 2

---
+++ Reto2.js
@@ -0,0 +1,19 @@
+// n una lucha épica entre muertos vivientes 🧟 y humanos 👮‍♂️, ambos bandos tienen una lista de combatientes con poderes de ataque específicos.
+
+// La batalla se desarrolla en rondas, y cada ronda enfrenta a cada combatiente de su bando.
+
+// El bando con mayor poder de ataque gana la ronda, y su poder se suma al siguiente combatiente de su equipo.
+
+// En caso de empate, ambos combatientes caen y no afectan a la próxima ronda.
+
+// Dadas dos cadenas de texto zombies y humans, donde cada dígito (del 1 al 9) representa el poder de ataque de un combatiente, 
+//determina quién queda al final y con cuánto poder de ataque.
+
+// Importante: Las dos cadenas siempre tendrán la misma longitud.
+
+// La salida es una cadena de texto que representa el resultado final de la batalla.
+
+// Si queda un zombie, devuelve su poder seguido de "z", por ejemplo "3z".
+// Si queda un humano, devuelve su poder seguido de "h", por ejemplo "2h".
+// Si hay un empate y ninguno queda con poder al final, devuelve "x".
+
